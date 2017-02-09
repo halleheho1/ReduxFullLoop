fd8a0a76a9c90f039a0ffce24052b3f4b2e1954c
halleheho <hallehehosg@hotmail.com>
2017-02-10 03:08:21 +0700

    Add v.1 of full loop react-redux app

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '00c0bdf1e02e80f83a0b63753dc560ad';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
